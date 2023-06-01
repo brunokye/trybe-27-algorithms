ec768a313e1bc473e4db8bc5d547870d73a3449e
Bruno Ferreira <contatobrunokye@gmail.com>
2023-06-01 17:36:23 -0300

    fix: ordem dos testes

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -3,12 +3,12 @@ from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    assert encrypt_message('Teste', 2) == 'ets_eT'
-
-    assert encrypt_message('Teste', 10) == 'etseT'
+    with pytest.raises(TypeError, match='tipo inválido para key'):
+        encrypt_message('Teste', 2.5)
 
     with pytest.raises(TypeError, match='tipo inválido para message'):
         encrypt_message(1, 2)
 
-    with pytest.raises(TypeError, match='tipo inválido para key'):
-        encrypt_message('Teste', 2.5)
+    assert encrypt_message('Teste', 10) == 'etseT'
+
+    assert encrypt_message('Teste', 2) == 'ets_eT'
