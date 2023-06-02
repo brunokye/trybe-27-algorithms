ffe12584105706b22373d3604ee5edc7b15a34e9
Bruno Ferreira <contatobrunokye@gmail.com>
2023-06-02 17:22:41 -0300

    feat: requisito 05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,11 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    seen = set()
+
+    for num in nums:
+        if isinstance(num, str) or num < 0:
+            return False
+        if num in seen:
+            return num
+        seen.add(num)
+
+    return False
